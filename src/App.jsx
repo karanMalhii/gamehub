26df58a6eeffc5aa23f786011cf0f532ea2e76ca
karanmalhii <karanmalhii1313@gmail.com>
2024-03-29 22:44:34 +0530

    use react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Button } from 'flowbite-react';
 import Navigation from "./Components/Navigation";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "flowbite-react";
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+});
 
 export default function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["games"],
+    queryFn: async function () {
+      const response = await axiosInstance.get(
+        "/games?key=0bbabdb992314fd3beab39914f8cb708&page=1"
+      );
+      console.log(response.data.results);
+      return response.data.results;
+    },
+  });
   return (
     <div>
-      <Navigation/>
+      <Navigation />
+      {isLoading && (<Spinner  size="lg" />)}
+      
+      {data?.map((item, index) => {
+        return <div key={index}>{item.name}</div>;
+      })}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const obj = new QueryClient();
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +16,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={obj}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
